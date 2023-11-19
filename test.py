ab3fc7931d556e10970261eba9d930ef4a6c277f
sorenblattbendtsen <s164521@student.dtu.dk>
2023-11-19 16:46:54 +0100

    unet_python_file

--- test.py
+++ test.py
@@ -13,10 +13,7 @@ import seaborn as sns
 #import torch.nn.init as init
 #from torch.nn import Linear, Conv2d, BatchNorm2d, MaxPool2d, Dropout2d
 #from torch.nn.functional import relu, elu, relu6, sigmoid, tanh, softmax
-from sklearn import metrics
 #from torchvision.transforms import ToTensor
-import rasterio
-from rasterio.plot import show
 import os
 from glob import glob
 from PIL import Image
@@ -25,3 +22,4 @@ import time
 
 my_list = np.ones(5)*5
 print(my_list)
+print("This works somehow?")
