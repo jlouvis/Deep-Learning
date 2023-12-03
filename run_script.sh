613f0c15a0de18f0fc285ebd0eacfa5f5e9dd0f9
jlouvis <s222556@dtu.dk>
2023-12-03 18:04:06 +0100

    chnges to unet for flip/overfitting

--- run_script.sh
+++ run_script.sh
@@ -10,7 +10,7 @@
 ### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
 ### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
-#BSUB -W 00:30
+#BSUB -W 00:20
 # request 5GB of system-memory
 #BSUB -R "rusage[mem=20GB]"
 ### -- set the email address --
